6c405804ff52616fa6b43a17aa334b43ab417ede
Ramla Eman <ramla.eman.ra@gmail.com>
2025-04-29 08:55:31 +0500

    add download cv button functionality

--- src/components/ui/DownloadCV.jsx
+++ src/components/ui/DownloadCV.jsx
@@ -2,9 +2,17 @@ import React from "react";
 import { MdOutlineFileDownload } from "react-icons/md";
 
 const DownloadCV = () => {
+  const downloadCV = () => {
+    const link = document.createElement("a");
+    link.href = "/Ramla Eman 2024.pdf";
+    link.download = "Ramla Eman 2024.pdf";
+    document.body.appendChild(link);
+    link.click();
+    document.body.removeChild(link);
+  };
   return (
     <div>
-      <button className="bg-button hover:bg-button-hover text-white py-2 px-6 rounded-full text-2xl flex items-center gap-2 font-semibold">
+      <button onClick={downloadCV} className="bg-button hover:bg-button-hover text-white py-2 px-6 rounded-full text-2xl flex items-center gap-2 font-semibold">
         <p>Download CV</p> <MdOutlineFileDownload />
       </button>
     </div>
